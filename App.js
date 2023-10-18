ec4640e3b3b1a002667c1c9450c7193c5aa7bf5a
Ferhat Ergün <123299806+ferhatergun@users.noreply.github.com>
2023-10-18 19:14:07 +0300

    tabbar yapısı oluşturuldu

--- App.js
+++ App.js
@@ -3,6 +3,9 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Profile from './src/pages/Profile';
 import Posts from './src/pages/Posts';
+import  IconIo  from 'react-native-vector-icons/Ionicons';
+import IconEn from 'react-native-vector-icons/Entypo';
+import { View } from 'react-native';
 
 
 
@@ -10,10 +13,38 @@ export default function App() {
   const Tab = createBottomTabNavigator();
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Posts" component={Posts} />
-        <Tab.Screen name="Home" component={Home} />
-        <Tab.Screen name="Profle" component={Profile} />
+      <Tab.Navigator
+      screenOptions={{
+        tabBarStyle:{backgroundColor:'green',height:60},
+        tabBarIconStyle:{},
+        tabBarItemStyle:{},
+        tabBarShowLabel:false
+      }}
+      >
+        <Tab.Screen name="Posts" component={Posts} 
+        options={{
+          tabBarIcon: () => (
+            <IconIo name="list-outline" color="white" size={30} />
+          ),
+        }}
+        />
+        <Tab.Screen name="Home" component={Home} 
+        options={{
+          tabBarIcon: () => (
+            <View style={{backgroundColor:'green'}} className="w-[60%] -top-3 h-20 rounded-[40px]  
+            flex justify-center items-center">
+              <IconEn name="ticket" color="green" size={40} style={{backgroundColor:'white',borderRadius:30,padding:10}}  />
+            </View>
+          ),
+        }}
+        />
+        <Tab.Screen name="Profle" component={Profile}
+        options={{
+          tabBarIcon: () => (
+            <IconIo name="person-outline" color="white" size={30} />
+          ),
+        }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
